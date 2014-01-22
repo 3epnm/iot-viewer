f53b6bfef0fca2e536746c3d1c79bfb9e720391b
Marcel Bretschneider <marcel.bretschneider@gmail.com>
2014-01-22 00:15:20 +0000

    Minifiy demo scripts and styles

---
+++ compile/demo.js
@@ -0,0 +1,190 @@
+// InternetOfThings Viewer 0.0.1
+
+// (c) 2014 Marcel Bretschneider
+// InternetOfThings Viewer may be freely distributed under the MIT license.
+// For all details and documentation:
+// http://mb.aquarius.uberspace.de
+
+
+/* lib/appcache.js */
+
+
+
+/* lib/date.js */
+
+
+
+/* contrib/jquery/jquery-1.9.1.js */
+
+
+
+/* contrib/jsonp/jsonp.js */
+
+
+
+/* contrib/hammerjs/hammer.js */
+
+
+
+/* contrib/hammerjs/hammer.fakemultitouch.js */
+
+
+
+/* contrib/bootstrap/js/bootstrap.js */
+
+
+
+/* contrib/underscore/underscore.js */
+
+
+
+/* contrib/backbone/backbone.js */
+
+
+
+/* contrib/backbone/backbone.localStorage.js */
+
+
+
+/* lib/backbone-overrides.js */
+
+
+
+/* lib/om/AppData.js */
+
+
+
+/* lib/om/AppDataList.js */
+
+
+
+/* lib/om/ProviderDriver.js */
+
+
+
+/* lib/om/ProviderDriverJsonP.js */
+
+
+
+/* lib/om/ProviderOpenSense.js */
+
+
+
+/* lib/om/ProviderThingSpeak.js */
+
+
+
+/* lib/om/ProviderXively.js */
+
+
+
+/* lib/om/ProviderDriverList.js */
+
+
+
+/* lib/om/Device.js */
+
+
+
+/* lib/om/DeviceList.js */
+
+
+
+/* lib/om/Provider.js */
+
+
+
+/* lib/om/ProviderList.js */
+
+
+
+/* lib/views/PromptView.js */
+
+
+
+/* lib/views/ConfirmView.js */
+
+
+
+/* lib/views/ErrorView.js */
+
+
+
+/* lib/views/AppView.js */
+
+
+
+/* lib/views/AppConfigView.js */
+
+
+
+/* lib/views/ProviderOpenSenseInitializeView.js */
+
+
+
+/* lib/views/ProviderThingSpeakInitializeView.js */
+
+
+
+/* lib/views/ProviderXivelyInitializeView.js */
+
+
+
+/* lib/views/DevicesView.js */
+
+
+
+/* lib/views/ProviderDriverView.js */
+
+
+
+/* lib/views/ProviderView.js */
+
+
+
+/* contrib/flot/jquery.flot.js */
+
+
+
+/* contrib/flot/jquery.flot.resize.js */
+
+
+
+/* contrib/flot/jquery.flot.crosshair.js */
+
+
+
+/* contrib/flot/jquery.flot.selection.js */
+
+
+
+/* contrib/flot/jquery.flot.time.js */
+
+
+
+/* lib/views/PlotView.js */
+
+
+
+/* contrib/drumjs/drum.js */
+
+
+
+/* lib/DateRange.js */
+
+
+
+/* lib/views/MenuView.js */
+
+
+
+/* contrib/panelmenu/panelmenu.js */
+
+
+
+/* lib/main.js */
+
+
+
+/* lib/views/TutorialView.js */
+
