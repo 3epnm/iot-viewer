978ed83de52f11133071181e1ae24d8c27eac695
Marcel Bretschneider <marcel.bretschneider@gmail.com>
2014-01-24 20:19:42 +0000

    Improved compilation, improved models

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,38 +1,55 @@
 #!/bin/bash
 
+# #############################
+# Collect scripts
+# #############################
+
 cat header.js > script.js
 
-for OUTPUT in $(/usr/bin/perl -lne '/<script src=\"(.*)\"/ and print "$1"' ../index.html)
+for OUTPUT in $(/usr/bin/perl -lne '/<script src=\"(.*)\"/ and print "$1"' compile.html)
 do
-	echo $OUTPUT
-	minify "../${OUTPUT}" "compile/${OUTPUT##*/}"
 	echo "" >> script.js
 	echo "" >> script.js
 	echo "/* **************************************************************************************************** */" >> script.js
 	echo "/* ${OUTPUT} */" >> script.js
 	echo "/* **************************************************************************************************** */" >> script.js
 	echo "" >> script.js
-	cat "compile/${OUTPUT##*/}" >> script.js
+	cat "../${OUTPUT}" >> script.js
 done
 
-minify script.js script.compile.js
+if [[ "$1" == "closure" ]]; then
+	# #############################
+	# Minify with google
+	# #############################
+	in=script.js
+	out=script.compile.js
 
-#curl -s \
-#	-d compilation_level=SIMPLE_OPTIMIZATIONS \
-#	-d output_format=text \
-#	-d output_info=compiled_code \
-#	--data-urlencode "js_code@${in}" \
-#	http://closure-compiler.appspot.com/compile \
-#	> script.compile.js
+	curl -s \
+		-d compilation_level=SIMPLE_OPTIMIZATIONS \
+		-d output_format=text \
+		-d output_info=compiled_code \
+		--data-urlencode "js_code@${in}" \
+		http://closure-compiler.appspot.com/compile \
+		> $out
+else
+	# #############################
+	# Minify with node
+	# #############################
+	minify script.js script.compile.js
+fi
 
 cat header.js > script.min.js
 cat script.compile.js >> script.min.js
+rm script.compile.js
+
+# #############################
+# Styles
+# #############################
 
 cat header.css > styles.css
 
 for OUTPUT in $(/usr/bin/perl -lne '/<link rel=\"stylesheet\" href=\"(.*)\"/ and print "$1"' ../index.html)
 do
-	echo $OUTPUT
 	cat "../${OUTPUT}" >> styles.css
 done
 
@@ -40,5 +57,4 @@ minify styles.css styles.compile.css
 cat header.css > styles.min.css
 cat styles.compile.css >> styles.min.css
 
-rm script.compile.js
 rm styles.compile.css
