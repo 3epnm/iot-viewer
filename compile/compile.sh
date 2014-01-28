614eaef48cbc3f865c7e95dce9867ce3a49c9d0e
Marcel Bretschneider <marcel.bretschneider@gmail.com>
2014-01-28 01:48:33 +0000

    Demo updated with 1.1beta

--- compile/compile.sh
+++ compile/compile.sh
@@ -1,44 +1,59 @@
 #!/bin/bash
 
+# #############################
+# Collect demos
+# #############################
+
 cat header.js > demo.js
 
 for OUTPUT in $(/usr/bin/perl -lne '/<script src=\"(.*)\"/ and print "$1"' compile.html)
 do
-	echo $OUTPUT
-	minify "../${OUTPUT}" "compile/${OUTPUT##*/}"
 	echo "" >> demo.js
 	echo "" >> demo.js
 	echo "/* **************************************************************************************************** */" >> demo.js
 	echo "/* ${OUTPUT} */" >> demo.js
 	echo "/* **************************************************************************************************** */" >> demo.js
 	echo "" >> demo.js
-	cat "compile/${OUTPUT##*/}" >> demo.js
+	cat "../${OUTPUT}" >> demo.js
 done
 
-minify demo.js demo.compile.js
+if [[ "$1" == "closure" ]]; then
+	# #############################
+	# Minify with google
+	# #############################
+	in=demo.js
+	out=demo.compile.js
 
-#curl -s \
-#	-d compilation_level=SIMPLE_OPTIMIZATIONS \
-#	-d output_format=text \
-#	-d output_info=compiled_code \
-#	--data-urlencode "js_code@${in}" \
-#	http://closure-compiler.appspot.com/compile \
-#	> script.compile.js
+	curl -s \
+		-d compilation_level=SIMPLE_OPTIMIZATIONS \
+		-d output_format=text \
+		-d output_info=compiled_code \
+		--data-urlencode "js_code@${in}" \
+		http://closure-compiler.appspot.com/compile \
+		> $out
+else
+	# #############################
+	# Minify with node
+	# #############################
+	minify demo.js demo.compile.js
+fi
 
 cat header.js > demo.min.js
 cat demo.compile.js >> demo.min.js
+rm demo.compile.js
+
+# #############################
+# demos
+# #############################
 
 cat header.css > demo.css
 
 for OUTPUT in $(/usr/bin/perl -lne '/<link rel=\"stylesheet\" href=\"(.*)\"/ and print "$1"' compile.html)
 do
-	echo $OUTPUT
 	cat "../${OUTPUT}" >> demo.css
 done
 
 minify demo.css demo.compile.css
-cat header.css > demo.min.css
 cat demo.compile.css >> demo.min.css
 
-rm demo.compile.js
 rm demo.compile.css
--- compile/header.js
+++ compile/header.js
@@ -1,4 +1,4 @@
-// InternetOfThings Viewer 0.0.1
+// InternetOfThings Viewer 1.1.0beta
 
 // (c) 2014 Marcel Bretschneider
 // InternetOfThings Viewer may be freely distributed under the MIT license.
